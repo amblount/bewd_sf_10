31cc98bd5f44d8dc4990b9f4a5f8dfd5bdf27c8e
amblount <amblount@ucdavis.edu>
2015-08-03 20:20:19 -0700

    completed string work

---
+++ 02_Ruby_Fundamentals_1/exercises/string_work.rb
@@ -0,0 +1,17 @@
+greeting = 'HEY GIRL, HEY!'
+name = 'Alivia'
+bewd_class = 'Back End Web Development'
+
+#call al methods
+puts name.length
+puts greeting.strip
+puts bewd_class.chars
+
+
+puts "#{name} has #{name.length} characters"
+
+puts "#{bewd_class} is a course that moves at a fast pace."
+
+puts "#{name.reverse} is my name backwards"
+
+puts "If you like "
